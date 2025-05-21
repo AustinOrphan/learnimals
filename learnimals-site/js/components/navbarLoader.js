4a74e0999f672cf4d384314ae62f241fb508ff72
Austin Orphan <39813920+AustinOrphan@users.noreply.github.com>
2025-05-21 18:09:24 -0500

    Fix pathing of shared navbar component

--- learnimals-site/js/components/navbarLoader.js
+++ learnimals-site/js/components/navbarLoader.js
@@ -1,6 +1,6 @@
 // js/components/navbarLoader.js
-fetch('../navbar.html')
-  .then(res => res.text())
-  .then(data => {
-    document.getElementById('navbar-placeholder').innerHTML = data;
+fetch("/learnimals-site/components/navbar.html")
+  .then((res) => res.text())
+  .then((data) => {
+    document.getElementById("navbar-placeholder").innerHTML = data;
   });
